60527911c1e23aa29933b8dff89ba0fea1755720
af2al <afzal.hussain@sailssoftware.com>
2024-01-18 10:31:53 +0530

    initialized

--- dataStruct/src/dataStruct/demo/bubbleSort.java
+++ dataStruct/src/dataStruct/demo/bubbleSort.java
@@ -14,7 +14,7 @@ public class bubbleSort {
 		  
 		  
 		  for(int i=0; i<arr.length; i++) {
-			  for(int j=1;j<arr.length-i;j++) {
+			  for(int j=1;j<arr.length;j++) {
 				  
 				  if (arr[j-1]>arr[j]) {
 					  int temp= arr[j];
