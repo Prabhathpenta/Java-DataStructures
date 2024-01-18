e9a16d00e42f31d47a34a2fb570d2d31eb7caf8a
af2al <afzal.hussain@sailssoftware.com>
2024-01-18 10:24:21 +0530

    Initialised

--- dataStruct/src/dataStruct/demo/bubbleSort.java
+++ dataStruct/src/dataStruct/demo/bubbleSort.java
@@ -3,33 +3,30 @@ package dataStruct.demo;
 public class bubbleSort {
 	 public static void main (String [] args) {
 		   
-		  int nums[] = {9,5,17,3,1,13,7};
-		  int temp=0;
+		  int arr[] = {9,5,17,3,1,13,7};
 		  
-		  System.out.println("before sorting");
-		  for(int num:nums) {
-			  System.out.print(num + " ");
+		  System.out.print("unsorted array is : " );
+		  for(int i=0; i<arr.length;i++) {
+		  System.out.print( arr[i] + " ");
 		  }
-		  
-		  for(int i=0; i<nums.length; i++) {
-			  for(int j=0;j<nums.length-1;j++) 
-				  {
-				  	if(nums[j]>nums[j+1])                  //9>5, nums[j]=j[0] and nums[j+1]=j[1]
-				  											//  nums[j+1]=1
-				  	{
-				  		temp = nums[j];						//temp=9  
-				  		nums[j] = nums[j+1];	 	 		//nums[0]=>5
-				  		nums[j+1] = temp;					//nums[j+1]=9
-				  	}
-			  }
-		  }
-		  
-		  System.out.println();
-		  System.out.println("After sorting");
-		  for(int num:nums) {
-			  System.out.print(num + " ");
-	
-}
 
+
+		  
+		  
+		  for(int i=0; i<arr.length; i++) {
+			  for(int j=1;j<arr.length-i;j++) {
+				  
+				  if (arr[j-1]>arr[j]) {
+					  int temp= arr[j];
+					  arr[j]=arr[j-1];
+					  arr[j-1]=temp;
+					   }
+				  }
+			}
+		  System.out.println();
+		  System.out.print("sorted array is : " );
+		  for(int i=0; i<arr.length;i++) {
+		  System.out.print( arr[i] + " ");
+		  }
 }
 }
