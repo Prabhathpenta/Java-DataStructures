a7602a2f6377a9079e4d8102f8d5c6ca5bb58eff
af2al <afzal.hussain@sailssoftware.com>
2024-01-18 11:40:26 +0530

    INITIALIZED

--- dataStruct/src/dataStruct/demo/bubbleSort.java
+++ dataStruct/src/dataStruct/demo/bubbleSort.java
@@ -3,18 +3,18 @@ package dataStruct.demo;
 public class bubbleSort {
 	 public static void main (String [] args) {
 		   
-		  int arr[] = {9,5,17,3,1,13,7};
+		  int arr[] = {9,5,17,3,1,13,7,4};
 		  
 		  System.out.print("unsorted array is : " );
-		  for(int i=0; i<arr.length;i++) {
-		  System.out.print( arr[i] + " ");
+		  for(int num:arr) {
+		  System.out.print( num + " ");
 		  }
 
 
 		  
 		  
 		  for(int i=0; i<arr.length; i++) {
-			  for(int j=1;j<arr.length;j++) {
+			  for(int j=1;j<arr.length-i;j++) {
 				  
 				  if (arr[j-1]>arr[j]) {
 					  int temp= arr[j];
@@ -25,8 +25,8 @@ public class bubbleSort {
 			}
 		  System.out.println();
 		  System.out.print("sorted array is : " );
-		  for(int i=0; i<arr.length;i++) {
-		  System.out.print( arr[i] + " ");
+		  for(int num:arr) {
+		  System.out.print( num + " ");
 		  }
 }
 }
